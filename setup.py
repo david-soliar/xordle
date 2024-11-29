50646c698332d265437883949d83c1bf0b87449f
David Soliar <dav.soliar@gmail.com>
2024-11-29 23:13:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="xordle_solver",
+    name="xordle",
     version="1.0.0",
     packages=find_packages(),
     python_requires=">=3.12",
